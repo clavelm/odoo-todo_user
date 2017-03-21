ec7fe2c426ddefb196c6ecc983028570ad9279ed
Mathieu CLAVEL <math.clavel@gmail.com>
2017-03-21 17:54:44 +0100

    add mail followers and thread to form view

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Multiuser To-Do',
     'description': 'Extends the To-Do app to multiuser.',
     'author': 'Mathieu CLAVEL',
-    'depends': ['todo_app',],
+    'depends': ['todo_app', 'mail', ],
     'data': [
         'views/todo_task.xml',
     ],
